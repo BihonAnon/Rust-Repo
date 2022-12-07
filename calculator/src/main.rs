61ab5c20c2c42a55225105dfbae93f41d24562ce
byron <byrondalberg@gmail.com>
2022-12-07 14:49:46 -0800

    println! macro (fcc8)

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -9,3 +9,17 @@ fn first_name() {
     //fcc 7 string concatenation VVV
     println!("My name is {}", first_name);
 }
+fn fcc8() {
+    //fcc 8
+    /*
+    println! is a macro that prints text to the console.
+    this makes it an excelent debugging tool, as you can
+    print the value of a variable at various points in your
+    code to see how it changes.
+    --DOCS-- https://doc.rust-lang.org/rust-by-example/hello/print.html
+    */
+    let first_name = "John";
+    let last_name = "Doe";
+    println!("My name is {} {}", first_name, last_name);
+
+}
